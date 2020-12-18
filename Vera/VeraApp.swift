292fbce2b035d9f1d2d6863f1124f995aa7bd141
Justin Cabral <ijustincabral@gmail.com>
2020-12-18 17:10:30 -0500

    Initial Commit

---
+++ Vera/VeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeraApp.swift
+//  Vera
+//
+//  Created by Justin Cabral on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct VeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
