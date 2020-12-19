6e7994c263e015f35feff5cee64a0da188602d96
Justin Cabral <ijustincabral@gmail.com>
2020-12-19 09:41:28 -0500

    Initial commit.

--- Vera/ContentView.swift
+++ Vera/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "pencil.slash")
+                    Text("Classes")
+                }
+            
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Image(systemName: "archivebox")
+                    Text("Finished")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+
+    
+    static var dataController = DataController.preview
+    
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
+
 }
---
+++ Vera/ProjectsView.swift
@@ -0,0 +1,50 @@
+//
+//  ProjectsView.swift
+//  Vera
+//
+//  Created by Justin Cabral on 12/18/20.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    
+    let showClosedProjects: Bool
+    let projects: FetchRequest<Project>
+    
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        
+        projects = FetchRequest<Project>(entity: Project.entity(),
+                                         sortDescriptors: [NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)],
+                                         predicate: NSPredicate(format: "closed = %d", showClosedProjects))
+        
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(projects.wrappedValue) { project in
+                    Section(header: Text(project.title ?? "")) {
+                        ForEach(project.items?.allObjects as? [Item] ?? []) { item in
+                            Text(item.title ?? "")
+                        }
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle(showClosedProjects ? "Finished Classes" : "Current Semester")
+        }
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+    
+    static var dataController = DataController.preview
+    
+    static var previews: some View {
+        ProjectsView(showClosedProjects: false)
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
